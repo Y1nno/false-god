5bb91a99a12a5adb6127320ed47ceddd76bcba43
Y1nno <59004110+Y1nno@users.noreply.github.com>
2026-01-20 20:59:33 -0600

    Update DungeonManager.cs
    Add DungeonManager initial implementation
    Add DungeonFloorData struct to hold floor/room data

--- Assets/Scripts/GameModules/DungeonManager/DungeonManager.cs
+++ Assets/Scripts/GameModules/DungeonManager/DungeonManager.cs
@@ -2,5 +2,51 @@ using UnityEngine;
 
 public class DungeonManager
 {
-    
+    private int k_maxDungeonFloor = 100;
+    private int k_roomsPerFloor = 10;
+
+    public int currentDungeonFloor { get; private set; } = 1;
+    public int roomAtCurrentFloor { get; private set; } = 1;
+
+    // Advance to the next room, and if necessary, the next floor
+    public DungeonFloorData AdvanceRoom()
+    {
+        roomAtCurrentFloor++;
+        if (roomAtCurrentFloor > k_roomsPerFloor)
+        {
+            AdvanceFloor();
+        }
+        return new DungeonFloorData(currentDungeonFloor, roomAtCurrentFloor);
+    }
+
+    // Advance to the next floor
+    private void AdvanceFloor()
+    {
+        if (IsDungeonComplete())
+        {
+            Debug.Log("Dungeon is complete.");
+            return;
+        }
+        currentDungeonFloor++;
+        roomAtCurrentFloor = 1;
+    }
+
+    // Check if the dungeon run is complete
+    public bool IsDungeonComplete()
+    {
+        return currentDungeonFloor >= k_maxDungeonFloor;
+    }
+}
+
+// Data structure for dungeon floor information
+public struct DungeonFloorData
+{
+    public int floor;
+    public int room;
+
+    public DungeonFloorData(int x, int y)
+    {
+        floor = x;
+        room = y;
+    }
 }
