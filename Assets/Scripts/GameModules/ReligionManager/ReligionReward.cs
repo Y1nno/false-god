46f8f25b23e26f348176e43998bbbca15282f0fb
Y1nno <59004110+Y1nno@users.noreply.github.com>
2026-02-25 21:32:02 -0600

    Convert Reward Types into SO's

--- Assets/Scripts/GameModules/ReligionManager/ReligionReward.cs
+++ Assets/Scripts/GameModules/ReligionManager/ReligionReward.cs
@@ -2,69 +2,124 @@ using UnityEngine;
 using System;
 
 [Serializable]
-public abstract class ReligionReward
+public abstract class ReligionRewardSO : ScriptableObject
 {
     public abstract void ApplyReward();
     public abstract void RemoveReward();
 }
 
-[Serializable]
-public enum ReligionRewardType
-{
-    StartOfCombatStatBuff,
-    StartOfCombatSecondaryStatBuff,
-    RandomItem,
-    SpecificItem,
-    StatPoints,
-    HealingMultiplier,
-    AilmentImmunity,
-    AilmentOnEnemy,
-    Passive,
-    FreeRepairAtBlackSmith,
-    UpgradeEquipment,
-    ManaRecovery,
-    StatBuff,
-}
-
-[Serializable]
-public struct ReligionRewardData
-{
-    public ReligionRewardType type;
-    public float value;
-}
-
-[Serializable]
-public struct PrimaryStatWithValue
+[CreateAssetMenu(fileName = "StartOfCombatStatBuff", menuName = "Scriptable Objects/Religion Rewards/Start Of Combat Stat Buff")]
+public class StartOfCombatStatBuffReligionRewardSO : ReligionRewardSO
 {
     public Stat stat;
     public float value;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
 }
 
-[Serializable]
-public struct SecondaryStatWithValue
+[CreateAssetMenu(fileName = "StartOfCombatSecondaryStatBuff", menuName = "Scriptable Objects/Religion Rewards/Start Of Combat Secondary Stat Buff")]
+public class StartOfCombatSecondaryStatBuffReligionRewardSO : ReligionRewardSO
 {
     public SecondaryStat stat;
     public float value;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
 }
 
-[Serializable]
-public struct SpecificItemRewardData
-{
-    public int itemID;
-}
-
-[Serializable]
-public struct RandomItemRewardData
+[CreateAssetMenu(fileName = "RandomItemReward", menuName = "Scriptable Objects/Religion Rewards/Random Item")]
+public class RandomItemReligionRewardSO : ReligionRewardSO
 {
     public EquipmentSlot slot;
     public Rarity rarity;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
 }
 
-[Serializable]
-public struct AilmentOnEnemyRewardData
+[CreateAssetMenu(fileName = "SpecificItemReward", menuName = "Scriptable Objects/Religion Rewards/Specific Item")]
+public class SpecificItemReligionRewardSO : ReligionRewardSO
+{
+    public int itemID;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "StatPointsReward", menuName = "Scriptable Objects/Religion Rewards/Stat Points")]
+public class StatPointsReligionRewardSO : ReligionRewardSO
+{
+    public int points;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "HealingMultiplierReward", menuName = "Scriptable Objects/Religion Rewards/Healing Multiplier")]
+public class HealingMultiplierReligionRewardSO : ReligionRewardSO
+{
+    public float multiplier;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "AilmentOnEnemyReward", menuName = "Scriptable Objects/Religion Rewards/Ailment On Enemy")]
+public class AilmentOnEnemyReligionRewardSO : ReligionRewardSO
 {
     public AilmentType ailmentType;
     public float chance;
     public int maxEnemies;
     public int encounterCooldown;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "PassiveReward", menuName = "Scriptable Objects/Religion Rewards/Passive")]
+public class PassiveReligionRewardSO : ReligionRewardSO
+{
+    public Passive passive;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "FreeRepairAtBlacksmithReward", menuName = "Scriptable Objects/Religion Rewards/Free Repair At Blacksmith")]
+public class FreeRepairAtBlacksmithReligionRewardSO : ReligionRewardSO
+{
+    public int freeRepairs;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "UpgradeEquipmentReward", menuName = "Scriptable Objects/Religion Rewards/Upgrade Equipment")]
+public class UpgradeEquipmentReligionRewardSO : ReligionRewardSO
+{
+    public int upgradeAmount;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "ManaRecoveryReward", menuName = "Scriptable Objects/Religion Rewards/Mana Recovery")]
+public class ManaRecoveryReligionRewardSO : ReligionRewardSO
+{
+    [Range(0f, 1f)]
+    public float manaRecoveryPercent;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
+}
+
+[CreateAssetMenu(fileName = "StatBuffReward", menuName = "Scriptable Objects/Religion Rewards/Stat Buff")]
+public class StatBuffReligionRewardSO : ReligionRewardSO
+{
+    public Stat stat;
+    public float value;
+
+    public override void ApplyReward() { }
+    public override void RemoveReward() { }
 }
--- Assets/Scripts/ScriptableObjects/Religions/ReligionSO.cs
+++ Assets/Scripts/ScriptableObjects/Religions/ReligionSO.cs
@@ -24,24 +24,5 @@ public class ReligionLevel
     public int requiredItemID;          // for GiveItem
     public int requiredItemAmount;      // for GiveItem
     public int bossID;                  // for KillBoss
-
-    // Reward
-    public ReligionRewardType rewardType;
-
-    // Reward params (fill based on rewardType)
-    public float value;        // DodgeChance (or general numeric)
-    public int specificItemID; // SpecificItem
-    public EquipmentSlot randomSlot;
-    public Rarity randomRarity;
-    public PrimaryStatWithValue startOfCombatStatBuff;
-    public SecondaryStatWithValue startOfCombatSecondaryStatBuff;
-    public int StatPointsToAllocate;
-    public float HealingMultiplier;
-    public AilmentType ailmentImmunity;
-    public AilmentOnEnemyRewardData ailmentOnEnemy;
-    public Passive passive;
-    public int FreeRepairsAtBlackSmith;
-    public int UpgradeEquipment;
-    public float manaRecoveryPercent;
-    public PrimaryStatWithValue statBuff;
+    public ReligionRewardSO reward;
 }
